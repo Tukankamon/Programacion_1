1fd62120b7383cb06713c7fe767a4d42f912264f
Tukankamon <antovedaros@gmail.com>
2025-02-26 17:49:21 +0100

    almost_done

--- busca_puntos/busca_puntos.cpp
+++ busca_puntos/busca_puntos.cpp
@@ -2,15 +2,26 @@
 //Cuenta las comas y los caracteres
 
 #include <iostream>
+#include <typeinfo>
 using namespace std;
 
 int main(){
     string text;
-    string dot = ".";
+    int comma_counter = 0;
+    int char_counter = 0;
     cin >> text;
     for (int i = 0; i < text.size(); i++){
-        if (text[i] == dot){
-            cout << "HOLa";
+        //cout << typeid(text[i]).name() << endl;
+        if (text[i] == *"."){
+            break;
+        }
+        else if (text[i] == *","){
+            comma_counter++;
+        }
+        else{
+            char_counter++;
         }
     }
+    cout << "Se han escrito " << char_counter << " caracteres \n";
+    cout << "Y " << comma_counter << " commas";
 }
